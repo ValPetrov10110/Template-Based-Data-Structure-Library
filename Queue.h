065d24d501322a58ab18003e6b8a034dfe333414
ValPetrov10110 <valpetrov.soccer@gmail.com>
2025-09-22 13:12:09 -0400

    Completed the skeleton of the Queue header file

--- Queue.h
+++ Queue.h
@@ -1,3 +1,39 @@
 #pragma once
 #include <vector>
 
+template <typename T>
+class Queue {
+private:
+    std::vector<T> container;
+
+public:
+    // Checks to see if the Queue is empty
+    bool isEmpty() {
+
+    }
+
+    // Returns the size of the Queue
+    size_t size() {
+
+    }
+
+    // Returns the value in the front of the Queue
+    const T& front() {
+
+    }
+
+    // Returns the value in the back of the Queue
+    const T& back() {
+
+    }
+
+    // Inserts an element in the back of the Queue
+    void push_back(const T& a) {
+
+    }
+
+    // Removes AND returns the first element in the Queue
+    const T& pop_front() {
+
+    }
+};
