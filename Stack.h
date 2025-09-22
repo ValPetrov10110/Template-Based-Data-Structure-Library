0598b5f2a9210a4d1a162e1aa73a3fd96427048a
ValPetrov10110 <valpetrov.soccer@gmail.com>
2025-09-21 21:12:31 -0400

    Added the header for the Stack ADT, constructed the outline for its functionality

---
+++ Stack.h
@@ -0,0 +1,43 @@
+/*
+ * This header file is in charge of the implementation of the
+ * Stack ADT
+ */
+
+#pragma once
+
+#include <vector>
+
+template <typename T>
+class Stack {
+
+// The underlying container for the data structure
+private:
+    std::vector<T> container;
+
+public:
+    // Inserts an element into the stack
+    void push(T& a) {
+
+    }
+
+    // Removes an element from the stack
+    void pop() {
+
+    }
+
+    // Returns the top element of the stack
+    T top() {
+
+    }
+
+    // Returns true if stack is empty, else false
+    bool isEmpty() {
+
+    }
+
+    // Returns the size of the stack
+    size_t size() {
+
+    }
+
+};
