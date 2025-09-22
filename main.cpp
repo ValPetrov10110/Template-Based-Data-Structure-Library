c37ae8b5d10256b3b7879f091612e5329f4bb178
ValPetrov10110 <valpetrov.soccer@gmail.com>
2025-09-21 22:54:10 -0400

    Tested the new implementation of size(); made sure const reference properly worked for push()

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 int main() {
     Stack<int> pushTest;
     std::cout << "Stack Empty: " << pushTest.isEmpty() << '\n';
+    std::cout << "Stack Size: " << pushTest.size() << '\n';
     pushTest.push(5);
     pushTest.push(6);
     pushTest.push(7);
     std::cout << "The top element is " << pushTest.top() << '\n';
     std::cout << "Stack Empty: " << pushTest.isEmpty() << '\n';
+    std::cout << "Stack Size: " << pushTest.size() << '\n';
     pushTest.print();
     pushTest.pop();
     std::cout << "\n";
